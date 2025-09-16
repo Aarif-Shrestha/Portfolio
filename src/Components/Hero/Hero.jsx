7c58aefc687fa8a4907b0e7cf7d549f7998fd525
Aarif Shrestha <aarifshrestha@gmail.com>
2025-09-16 14:16:23 +0545

    Responsive done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/pp.png'
 
+
 const Hero = () => {
   return (
     <div>
@@ -11,8 +12,8 @@ const Hero = () => {
         <h1><span>I'm Aarif Shrestha</span>, an aspiring and passionate developer.</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam aperiam sapiente dolore error! Ex dolore sunt, doloribus eveniet et ea!</p>
         <div className='hero-action'>
-            <div className="resume">Resume</div>
-            <div className="linkedin">LinkedIn</div>
+            <a style={{ textDecoration: 'none', color: 'white' }} href="https://drive.google.com/file/d/1qCSgH97FwOzkOf6DpmP44zYZg4Cqegdq/view?usp=sharing" offset={50} target='_blank'><div className="resume">Resume</div></a>
+            <a style={{ textDecoration: 'none', color: 'white' }} href="https://www.linkedin.com/in/aarif-shrestha/" offset={50} target='_blank'><div className="linkedin">LinkedIn</div></a>
 
         </div>
       </div>
